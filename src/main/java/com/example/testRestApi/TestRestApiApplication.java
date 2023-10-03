d3ff0d58666e7b50e31b922588d64e4158c155d4
Nikita <nikitamasslennikov@mail.ru>
2023-10-03 17:53:46 +0300

    Initial commit

---
+++ src/main/java/com/example/testRestApi/TestRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.testRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRestApiApplication.class, args);
+	}
+
+}
